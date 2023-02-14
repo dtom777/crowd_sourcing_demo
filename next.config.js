10e4f86b3424b5153fbb09f5c30e322ef41c26bf
dtom777 <ethan230204@gmail.com>
2023-02-15 01:52:45 +0900

    update:config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,9 @@
-module.exports = {
-  images: {
-    domains: ['abs.twimg.com', 'scontent.fkix2-2.fna.fbcdn.net'],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      child_process: false,
-      net: false,
-      dns: false,
-      tls: false,
-    };
-
-    return config;
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   publicRuntimeConfig: {
     WEBAPP_URL: process.env.WEBAPP_URL,
-    FACEBOOK_ID: process.env.FACEBOOK_ID,
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
   },
 };
+
+module.exports = nextConfig;
